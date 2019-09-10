489826d29ea761d2b0d3ff1e6fc397f0c7f39210
phongsiri-F <55137279+phongsiri-F@users.noreply.github.com>
2019-09-10 18:03:51 +0700

    Add files via upload

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace laout.Controllers
+{
+    public class IndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DemoCard()
+        {
+            return View();
+        }
+    }
+}
